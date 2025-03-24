9d16c648c7bc5f7674271ebfe3c9361c9cfe4cb1
Lay Nath <nath.selendra@gmail.com>
2025-03-24 10:01:11 +0700

    update contract controller

---
+++ src/utils/contract/abi/factory.ts
@@ -0,0 +1,22 @@
+export const ERC20FACTORY_ABI = [
+  "function isTokenCreatedByFactory(address token) external view returns (bool)",
+  "function tokenRatios(address token) external view returns (uint256)",
+  "function stableCoin() external view returns (address)",
+  "event TokenCreated(address indexed creator, address indexed tokenAddress, string name, string symbol, address tokenOwner)",
+  "event TokenMinted(address indexed tokenAddress, address indexed to, uint256 amount)",
+  "event TokenRatioSet(address indexed tokenAddress, uint256 tokensPerStableCoin)",
+  "event StableCoinAddressSet(address stableCoinAddress)",
+];
+
+export const ERC20FACTORY_ADMIN_ABI = [
+  "function createToken(string memory name, string memory symbol, address tokenOwner, uint256 tokensPerStableCoin) external returns (address)",
+  "function mintToken(address tokenAddress, address to, uint256 amount) external",
+  "function isTokenCreatedByFactory(address token) external view returns (bool)",
+  "function tokenRatios(address token) external view returns (uint256)",
+  "function stableCoin() external view returns (address)",
+  "function getAllTokenAddresses() external view returns (address[])",
+  "event TokenCreated(address indexed creator, address indexed tokenAddress, string name, string symbol, address tokenOwner)",
+  "event TokenMinted(address indexed tokenAddress, address indexed to, uint256 amount)",
+  "event TokenRatioSet(address indexed tokenAddress, uint256 tokensPerStableCoin)",
+  "event StableCoinAddressSet(address stableCoinAddress)",
+];
