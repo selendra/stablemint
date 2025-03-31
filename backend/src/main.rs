f11a42ffa26ac972b14c61269dafb2031bd75ca3
LayNath242 <nath.selendra@gamail.com>
2025-03-31 15:27:44 +0700

    new

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,14 +1,42 @@
 use async_graphql::{http::GraphiQLSource, EmptyMutation, EmptySubscription, Object, Schema};
 use async_graphql_axum::{GraphQLRequest, GraphQLResponse};
-use axum::{response::{Html, IntoResponse}, routing::get, Extension, Router};
+use axum::{
+    response::{Html, IntoResponse},
+    routing::get,
+    Extension, Router,
+};
+use tower_http::{
+    cors::{Any, CorsLayer},
+};
+
 
 #[tokio::main]
 async fn main() {
+    // Create the Schema
+    let schema = Schema::build(Query::default(), EmptyMutation, EmptySubscription)
+        .finish();
     
     let router = Router::new()
-    .route("/graphql", get(graphql_playground).post(graphql_handler));
+        .route("/graphql", get(graphql_playground).post(graphql_handler))
+        // Add the schema as an extension
+        .layer(Extension(schema))
+        .layer(
+            CorsLayer::new()
+                .allow_origin(Any)
+                .allow_headers([
+                    axum::http::header::CONTENT_TYPE,
+                    axum::http::header::AUTHORIZATION,
+                ])
+                .allow_methods([
+                    axum::http::Method::GET,
+                    axum::http::Method::POST,
+                    axum::http::Method::OPTIONS,
+                ]),
+        );
     
-    let listener = tokio::net::TcpListener::bind("localhost:3000").await.unwrap();
+    println!("GraphQL playground available at: http://localhost:8000/graphql");
+    
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
     axum::serve(listener, router).await.unwrap();
 }
 
@@ -19,20 +47,17 @@ async fn graphql_playground() -> impl IntoResponse {
 // Type alias for our GraphQL schema
 type ApiSchema = Schema<Query, EmptyMutation, EmptySubscription>;
 
-// Handler for GraphQL POST requests with authentication
+// Handler for GraphQL POST requests
 async fn graphql_handler(
     schema: Extension<ApiSchema>,
     req: GraphQLRequest,
 ) -> Result<GraphQLResponse, ()> {
     let req_builder = req.into_inner();
-
-    // Execute the GraphQL request
     let response = schema.execute(req_builder).await;
-
     Ok(response.into())
 }
 
-
+#[derive(Default)]
 struct Query;
 
 #[Object]
