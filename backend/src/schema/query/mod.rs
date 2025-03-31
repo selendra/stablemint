5c0ea67fb8c8666d33e6697035f85b4816294c6c
LayNath242 <nath.selendra@gamail.com>
2025-03-31 09:46:49 +0700

    remove

--- backend/src/schema/query/mod.rs
+++ backend/src/schema/query/mod.rs
@@ -1,11 +1,10 @@
-pub mod hello;
 pub mod user;
 
 use async_graphql::MergedObject;
 
 #[derive(MergedObject)]
-pub struct Query(hello::HelloQuery, user::UserQuery);
+pub struct Query(user::UserQuery);
 
 pub fn create_query() -> Query {
-    Query(hello::HelloQuery, user::UserQuery)
+    Query( user::UserQuery)
 }
