ae2bf30b33c2e5332b31d267afda021ad1606f51
Nath <nath.selendra@gmail.com>
2025-04-02 08:15:01 +0700

    update contract roken

--- backend/models/src/token.rs
+++ backend/models/src/token.rs
@@ -1,12 +1,12 @@
+use std::str;
+
 use async_graphql::{SimpleObject, ID};
 use chrono::{DateTime, Utc};
 use serde::{Serialize, Deserialize};
 
-use crate::user::User;
-
 // Token balance structure (simplified for the user service)
 #[derive(SimpleObject, Clone, Serialize, Deserialize, Debug)]
-pub struct TokenBalance {
+pub struct Token {
     pub contract_address: String,
     pub balance: String,
 }
@@ -17,14 +17,20 @@ pub struct TokenProfile {
     #[serde(skip_serializing_if = "Option::is_none")]
     pub id: Option<ID>,
     pub user_id: ID,
+    pub token: Vec<Token>,
     pub native_balance: String,
     pub created_at: DateTime<Utc>,
     pub updated_at: DateTime<Utc>,
 }
 
-// Authentication response type
-#[derive(SimpleObject)]
-pub struct AuthPayload {
-    pub token: String,
-    pub user: User,
+pub enum TokenType {
+    ERC20,
+    ERC721 
+}
+
+pub struct Contract {
+    pub user_id: ID,
+    pub contract_address: String,
+    pub created_at: String,
+    pub contract_type: TokenType,
 }
